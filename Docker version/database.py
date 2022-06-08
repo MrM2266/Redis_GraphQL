200b430f7ddd5f027e2cf64cc5764b2a0a0457f1
MrM2266 <re.marek@outlook.com>
2022-06-08 22:25:04 +0200

    FastAPI + docker

---
+++ Docker version/database.py
@@ -0,0 +1,305 @@
+import redis
+import data
+
+class CTable:
+    def __init__(self, name, db_no):
+        self.name = name
+        self.next_id = 1 #první volné id pro nový záznam
+        self.lines = 0 #počet řádků v tabulce
+        self.db_no = db_no
+        self.table = redis.Redis(host="RedisDB", port=6379, db = db_no, charset="utf-8", decode_responses=True)
+
+    def AddLine(self, id, data): #pod dane id ulozi dic v data
+        self.table.hset(id, mapping=data)
+        self.lines += 1
+
+    def AddRecord(self, record): #přidá záznam do tabulky - přiřadí mu první volné id
+        id = self.next_id
+        self.table.hset(id, mapping=record)
+        self.next_id += 1
+        self.lines += 1
+        return id
+        #record je dict, který obsahuje stringy - roles je string - json
+
+    def GetTableName(self):
+        return self.name
+
+    def GetKeys(self):
+        keys = self.table.keys()
+
+        for i in range(0, len(keys)):
+            keys[i] = int(keys[i])
+        return keys
+
+    def GetLine(self, id):
+        data = self.table.hgetall(id)
+        data["id"] = id
+        return data
+
+    def GetItem(self, id, item): #vrací string - pokud položka nebo id neecistuje - vrací None
+        return self.table.hget(id, item)
+
+    def UpdateItem(self, id, item, new_value): #pokud neexistuje, tak vytvoří
+        self.table.hset(id, item, new_value)
+
+    def DelItem(self, id, item): #vymaže položku na zadaném řádku
+        self.table.hdel(id, item)
+
+    def DelLine(self, id):
+        self.table.delete(id)
+        self.lines -=1
+    
+    def Populate(self, data): #bere data, dic, kde v každé položce je na prvním místě id
+        for line in data:
+            del line["id"]
+            self.AddRecord(line)
+
+    def ShowLine(self, id):
+        print(self.GetLine(id))
+
+def PopulateDB():
+    #naplní databázi cvičnými daty z data.py
+    users.Populate(data.data_users)
+    groups.Populate(data.data_groups)
+    users_groups.Populate(data.data_users_groups)
+    group_types.Populate(data.data_groupTypes)
+    role_types.Populate(data.data_roleTypes)
+
+
+def Relation11(table1, table2, table1_id): #1:1 relace
+    return table1.GetLine(table2.GetItem(table1_id, table1.GetTableName() + "_id")) #když je klic u user - tzn, user má car_id
+
+
+def Relation1N(table1, table2, table1_id): #1:N relace - klíč je na straně N
+    ids = table2.GetKeys()
+    output = []
+
+    for id in ids:
+        entity_id = int(table2.GetItem(id, table1.GetTableName() + "_id"))
+
+        if (entity_id == table1_id):
+            line = table2.GetLine(id)
+            line["id"] = id
+            output.append(line)
+
+    if (len(output) != 0): return output
+    else: return None
+
+
+def RelationMN(table1, interTable, table1_id):
+    #sestaví si název klíče podle table1.name
+    #vrací všechny řádky z interTable, které jako value pro sestavený klíč obsahují table1_id
+    #pro RelationMN(groups, users_groups, 1) vrací
+    #[{'users_id': '2', 'groups_id': '1', 'id': 4}, {'users_id': '3', 'groups_id': '1', 'id': 6}, {'users_id': '4', 'groups_id': '1', 'id': 8}, {'users_id': '1', 'groups_id': '1', 'id': 1}]
+    if(table1_id == None): return None
+    else: table1_id = int(table1_id)
+    line_ids = interTable.GetKeys()
+    output = []
+
+    for line_id in line_ids:
+        if (int(interTable.GetItem(line_id, table1.GetTableName() + "_id")) == table1_id):
+            output.append(interTable.GetLine(line_id))
+
+    if (len(output) != 0): return output
+    else: return None
+
+
+
+class Group:
+    def __init__(self, name = None, groupType = None):
+        #to co není list lze nastavit přes konstruktor
+        #ke stringům se přistupuje přímo - k listům pomocí fcí
+        self.id = None #toto budou jen stringy - obsah databáze
+        self.name = name
+        self.groupType = groupType
+        #members - pomocí GetMembers()
+        #groupType - pomocí GetGroupType
+
+    def AddToDB(self): #musíme popsat všechny sloupce tabulky v db kromě id (automaticky tvořeno)
+        #přidá tuto skupinu do databáze pod nové id
+        data = {
+            "name":str(self.name),
+            "groupType": str(self.groupType)}
+        self.id = groups.AddRecord(data)
+
+    def Load(self, group_id):
+        self.id = group_id
+        self.name = groups.GetItem(group_id, "name")
+        self.groupType = groups.GetItem(group_id, "groupType")
+
+    def LoadMembers(self):
+        #vrací pole stringů - jde o id uživatelů, kteří patří do této skupiny
+        #např. ['4', '2', '3', '1'] pro skupinu 1
+        data = RelationMN(groups, users_groups, self.id)
+
+        if (data != None):
+            out = []
+            for member in data:
+                out.append(str(member["users_id"]))
+            return out
+        else: return None
+
+    def UpdateData(self): #aktualizuje všechny string položky - uloží do databáze
+        groups.UpdateItem(self.id, "name", self.name)
+        groups.UpdateItem(self.id, "groupType", self.groupType)
+
+    def GetGroupType(self):
+        #v proměnné self.groupType mám teď objekt groupType - chtěl bych tam mít string
+        # který bude obsahovat id příslušného objektu GroupType
+        #vrací objekt GroupType pro tuto skupinu - lze na něm volat name atd.
+        gt = GroupType()
+        if (self.groupType != None): gt.Load(int(self.groupType))
+        return gt
+
+    def GetMembers(self):
+        #vrací list objektů Person, kteří jsou členy této skupiny
+        membersIDs = self.LoadMembers()
+        out = []
+
+        if(membersIDs != None):
+            for id in membersIDs:
+                tmp = Person()
+                tmp.Load(int(id))
+                out.append(tmp)
+        else:
+            tmp = Person()
+            out.append(tmp)
+
+        return out
+
+    def AddMember(self, member_id, roleType_id):
+        users_groups.AddRecord({"users_id":str(member_id), "groups_id":str(self.id), "roleType_id":roleType_id})
+
+    def RemoveMember(self, member_id):
+        data = RelationMN(groups, users_groups, self.id)
+        if (data != None):
+            for rec in data:
+                if(int(rec["users_id"]) == int(member_id)):
+                    users_groups.DelLine(int(rec["id"]))
+
+    def Del(self):
+        data = RelationMN(groups, users_groups, self.id)
+        if (data != None):
+            for rec in data:
+                users_groups.DelLine(int(rec["id"]))
+        if(self.id != None): groups.DelLine(int(self.id))
+
+
+class Membership: #relace mezi uživatelem a skupinou
+    def __init__(self, roleType_id = None, group_id = None):
+        self.roleType_id = roleType_id #string
+        self.group_id = group_id #string
+
+    def GetGroup(self):
+        gr = Group()
+        if(self.group_id != None): gr.Load(int(self.group_id))
+        return gr
+
+    def GetRoleType(self):
+        roleType = RoleType()
+        if(self.roleType_id != None): roleType.Load(int(self.roleType_id))
+        return roleType
+
+
+class Person:
+    def __init__(self, name = None, surname = None, age = None):
+        self.id = None
+        self.name = name
+        self.surname = surname
+        self.age = age
+        #memberships - pomocí GetMemberships()
+
+    def AddToDB(self):
+        data = {
+            "name":str(self.name),
+            "surname":str(self.surname),
+            "age":str(self.age)}
+
+        self.id = users.AddRecord(data)
+
+    def Load(self, user_id): #načte data z databáze
+        self.id = user_id
+        self.name = users.GetItem(user_id, "name")
+        self.surname = users.GetItem(user_id, "surname")
+        self.age = users.GetItem(user_id, "age")
+
+    def UpdateData(self): #aktualizuje všechny string položky - uloží do databáze
+        users.UpdateItem(self.id, "name", self.name)
+        users.UpdateItem(self.id, "surname", self.surname)
+        users.UpdateItem(self.id, "age", self.age)
+
+    def GetMemberships(self):
+        data = RelationMN(users, users_groups, self.id)
+        out = []
+
+        if(data != None):
+            for mem in data:
+                tmp = Membership()
+                tmp.group_id = mem["groups_id"]
+                tmp.roleType_id = mem["roleType_id"]
+                out.append(tmp)
+        else:
+            tmp = Membership()
+            out.append(tmp)
+
+        return out
+
+    def AddToGroup(self, group_id, roleType_id):
+        users_groups.AddRecord({"users_id":self.id, "groups_id":group_id, "roleType_id":roleType_id})
+
+    def Del(self):
+        data = RelationMN(users, users_groups, self.id)
+        if (data != None):
+            for rec in data:
+                users_groups.DelLine(int(rec["id"]))
+        if(self.id != None): users.DelLine(int(self.id))
+
+
+class GroupType:
+    def __init__(self, name = None):
+        self.id = None
+        self.name = name
+
+    def AddToDB(self):
+        data = {
+            "name":str(self.name)}
+
+        self.id = group_types.AddRecord(data)
+
+    def Load(self, groupType_id):
+        self.id = int(groupType_id)
+        self.name = group_types.GetItem(self.id, "name")
+
+    def UpdateData(self): #aktualizuje všechny string položky - uloží do databáze
+        group_types.UpdateItem(self.id, "name", self.name)
+
+    def Del(self):
+        if(self.id != None): group_types.DelLine(int(self.id))
+
+
+class RoleType:
+    def __init__(self, name = None):
+        self.id = None
+        self.name = name
+
+    def AddToDB(self):
+        data = {"name":str(self.name)}
+        self.id = role_types.AddRecord(data)
+
+    def Load(self, roleType_id):
+        self.id = roleType_id
+        self.name = role_types.GetItem(self.id, "name")
+
+    def UpdateData(self):
+        role_types.UpdateItem(self.id, "name", self.name)
+
+    def Del(self):
+        if(self.id != None): role_types.DelLine(int(self.id))
+
+
+
+users = CTable("users", 0)
+groups = CTable("groups", 1)
+users_groups = CTable("users_groups", 2)
+group_types = CTable("group_types", 3)
+role_types = CTable("role_types", 4)
---
+++ Docker version/gql.py
@@ -0,0 +1,334 @@
+import graphene
+import database
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+from fastapi import FastAPI
+
+class Person(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    surname = graphene.String()
+    age = graphene.String()
+    memberships = graphene.List(lambda: Membership)
+
+    def resolve_name(parent, info):
+        return str(parent.name)
+
+    def resolve_surname(parent, info):
+        return str(parent.surname)
+
+    def resolve_age(parent, info):
+        return str(parent.age)
+
+    def resolve_memberships(parent, info):
+        return parent.GetMemberships()
+
+class CreatePerson(graphene.Mutation):
+    person = graphene.Field(Person) #to co bude na výstupu mutace - to co bude vracet
+
+    class Arguments:
+        name = graphene.String()
+        surname = graphene.String()
+        age = graphene.String()
+
+    def mutate(parent, info, name, surname, age):
+        person = database.Person()
+        if(type(name) == str): person.name = name
+        if(type(surname) == str): person.surname = surname
+        if(type(age) == str): person.age = age
+        person.AddToDB()
+        return CreatePerson(person=person)
+
+class UpdatePerson(graphene.Mutation):
+    person = graphene.Field(Person)
+
+    class Arguments:
+        id = graphene.ID(required = True)
+        name = graphene.String()
+        surname = graphene.String()
+        age = graphene.String()
+
+    def mutate(parent, info, id, name, surname, age):
+        person = database.Person()
+        person.Load(int(id))
+        if(type(name) == str): person.name = name
+        if(type(surname) == str): person.surname = surname
+        if(type(age) == str): person.age = age
+        person.UpdateData()
+        return UpdatePerson(person = person)
+
+class DelPerson(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.ID(required = True)
+
+    def mutate(parent, info, id):
+        person = database.Person()
+        person.Load(int(id))
+        person.Del()
+        return DelPerson(status = "ok")
+
+
+class Group(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+    groupType = graphene.Field(lambda:GroupType) #id odpovídající groupType
+    members = graphene.List(Person)
+
+    def resolve_name(parent, info):
+        return str(parent.name)
+    
+    def resolve_groupType(parent, info):
+        return parent.GetGroupType()
+
+    def resolve_members(parent, info):
+        return parent.GetMembers()
+
+class CreateGroup(graphene.Mutation):
+    group = graphene.Field(Group)
+
+    class Arguments:
+        name = graphene.String()
+        groupTypeID = graphene.String()
+
+    def mutate(parent, info, name, groupTypeID):
+        group = database.Group()
+        if(type(name) == str): group.name = name
+        if(type(groupTypeID) == str): group.groupType = groupTypeID
+        group.AddToDB()
+        return CreateGroup(group=group)
+
+class UpdateGroup(graphene.Mutation):
+    group = graphene.Field(Group)
+
+    class Arguments:
+        id = graphene.ID(required = True)
+        name = graphene.String()
+        groupTypeID = graphene.String()
+
+    def mutate(root, info, id, name, groupTypeID):
+        group = database.Group()
+        group.Load(int(id))
+        if(type(name) == str): group.name = name
+        if(type(groupTypeID) == str): group.groupType = groupTypeID
+        group.UpdateData()
+        return UpdateGroup(group = group)
+
+class DelGroup(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.ID(required = True)
+
+    def mutate(parent, info, id):
+        gr = database.Group()
+        gr.Load(int(id))
+        gr.Del()
+        return DelGroup(status = "ok")
+
+
+class GroupType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+    def resolve_name(parent, info):
+        return str(parent.name)
+
+class CreateGroupType(graphene.Mutation):
+    groupType = graphene.Field(GroupType)
+
+    class Arguments:
+        name = graphene.String()
+
+    def mutate(parent, info, name):
+        groupType = database.GroupType()
+        if(type(name) == str): groupType.name = name
+        groupType.AddToDB()
+        return CreateGroupType(groupType=groupType)
+
+class UpdateGroupType(graphene.Mutation):
+    groupType = graphene.Field(GroupType)
+
+    class Arguments:
+        id = graphene.ID(required = True)
+        name = graphene.String()
+
+    def mutate(parent, info, id, name):
+        gt = database.GroupType()
+        gt.Load(int(id))
+        if(type(name) == str): gt.name = name
+        gt.UpdateData()
+        return UpdateGroupType(groupType = gt)
+
+class DelGroupType(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.ID(required = True)
+
+    def mutate(parent, info, id):
+        gt = database.GroupType()
+        gt.Load(int(id))
+        gt.Del()
+        return DelGroupType(status = "ok")
+
+class RoleType(graphene.ObjectType):
+    id = graphene.ID()
+    name = graphene.String()
+
+    def resolve_name(parent, info):
+        return str(parent.name)
+
+class CreateRoleType(graphene.Mutation):
+    roleType = graphene.Field(RoleType)
+
+    class Arguments:
+        name = graphene.String()
+
+    def mutate(parent, info, name):
+        roleType = database.RoleType()
+        if(type(name) == str): roleType.name = name
+        roleType.AddToDB()
+        return CreateRoleType(roleType=roleType)
+
+class UpdateRoleType(graphene.Mutation):
+    roleType = graphene.Field(RoleType)
+
+    class Arguments:
+        id = graphene.ID(required = True)
+        name = graphene.String()
+
+    def mutate(parent, info, id, name):
+        rt = database.RoleType()
+        rt.Load(int(id))
+        if(type(name) == str): rt.name = name
+        rt.UpdateData()
+        return UpdateRoleType(roleType = rt)
+
+class DelRoleType(graphene.Mutation):
+    status = graphene.String()
+
+    class Arguments:
+        id = graphene.ID(required = True)
+
+    def mutate(parent, info, id):
+        rt = database.RoleType()
+        rt.Load(int(id))
+        rt.Del()
+        return DelRoleType(status = "ok")
+
+class Membership(graphene.ObjectType):
+    roleType = graphene.Field(RoleType)
+    group = graphene.Field(Group)
+
+    def resolve_group(parent, info):
+        return parent.GetGroup()
+
+    def resolve_roleType(parent, info):
+        return parent.GetRoleType()
+
+class AddUserToGroup(graphene.Mutation):
+    group = graphene.Field(Group)
+    person = graphene.Field(Person)
+
+    class Arguments:
+        groupID = graphene.String()
+        userID = graphene.String()
+        roleTypeID = graphene.String()
+
+    def mutate(parent, info, groupID, userID, roleTypeID):
+        group = database.Group()
+        group.Load(int(groupID))
+        group.AddMember(userID, roleTypeID)
+        user = database.Person()
+        user.Load(int(userID))
+        return AddUserToGroup(group = group, person = user)
+
+class RemoveUserFromGroup(graphene.Mutation):
+    group = graphene.Field(Group)
+    person = graphene.Field(Person)
+
+    class Arguments:
+        groupID = graphene.String()
+        userID = graphene.String()
+
+    def mutate(parent, info, groupID, userID):
+        gr = database.Group()
+        gr.Load(int(groupID))
+        gr.RemoveMember(int(userID))
+        per = database.Person()
+        per.Load(int(userID))
+        return RemoveUserFromGroup(group = gr, person = per)
+
+
+class Query(graphene.ObjectType):
+    person = graphene.Field(Person, id = graphene.ID(required=True))
+    group = graphene.Field(Group, id = graphene.ID(required=True))
+    groupType = graphene.Field(GroupType, id = graphene.ID(required = True))
+    roleType = graphene.Field(RoleType, id = graphene.ID(required = True))
+
+    control = graphene.String(command = graphene.String()) #pouze pro školní účely
+
+    def resolve_person(root, info, id):
+        person = database.Person()
+        person.Load(id)
+        return person
+
+    def resolve_group(root, info, id):
+        group = database.Group()
+        group.Load(id)
+        return group
+
+    def resolve_groupType(root, info, id):
+        groupType = database.GroupType()
+        groupType.Load(id)
+        return groupType
+
+    def resolve_roleType(root, info, id):
+        roleType = database.RoleType()
+        roleType.Load(id)
+        return roleType
+
+    def resolve_control(root, info, command):
+        if (str(command) == "PopulateDB"): 
+            database.PopulateDB()
+            return "db populated from data.py"
+
+        if (str(command) == "EraseDB"): 
+            database.users.table.flushall()
+            return "db erased"
+
+        return "unknown command"
+
+
+class Mutations(graphene.ObjectType):
+    create_person = CreatePerson.Field()
+    create_group = CreateGroup.Field()
+    create_group_type = CreateGroupType.Field()
+    create_role_type = CreateRoleType.Field()
+
+    add_user_to_group = AddUserToGroup.Field()
+    remove_user_from_group = RemoveUserFromGroup.Field()
+    
+    update_person = UpdatePerson.Field()
+    update_group = UpdateGroup.Field()
+    update_group_type = UpdateGroupType.Field()
+    update_role_type = UpdateRoleType.Field()
+
+    del_person = DelPerson.Field()
+    del_group = DelGroup.Field()
+    del_group_type = DelGroupType.Field()
+    del_role_type = DelRoleType.Field()
+
+
+
+schemaGQL = graphene.Schema(query=Query, mutation=Mutations)
+#print(schema)
+
+#result = schema.execute(input.ctl2)
+#print(result)
+
+graphql_app = GraphQLApp(schemaGQL, on_get=make_graphiql_handler())
+
+app = FastAPI()
+app.add_route('/gql/', graphql_app)
